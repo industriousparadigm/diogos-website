47ea7de1f8491017f5194afa4c501f9e0cfc420c
Diogo Costa <dsgmcosta@gmail.com>
2021-04-18 16:19:41 +0100

    target serverless in next.config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const withMDX = require('@next/mdx')({
 });
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx']
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  target: 'serverless'
 });
